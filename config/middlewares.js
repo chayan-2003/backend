7b3456b0a013d96bfe8207485c92c82b8c950f64
Your Name <your.email@example.com>
2025-02-16 01:02:44 +0530

    mid

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,14 +1,21 @@
 module.exports = [
-
+  'strapi::errors',
+  'strapi::security',
+  'strapi::cors',
+  'strapi::poweredBy',
+  'strapi::logger',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
   {
     name: 'strapi::cors',
     config: {
-      enabled: true,
       origin: ['*'], // Allows all origins; replace '*' with specific domains in production for security
       methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
       headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
       keepHeaderOnError: true,
     },
   },
-
 ];
